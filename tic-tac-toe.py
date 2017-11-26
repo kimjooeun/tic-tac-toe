eed49f7f296e1172c7132a3dcb7fe1d41b36c674
kimjooeun <rlawndms222@naver.com>
2017-11-26 16:57:49 +0900

    60162110

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox
 
 def checked(i) :
       global player
@@ -15,10 +16,33 @@ def checked(i) :
       else :
             player = "X"
             button["bg"] = "lightgreen"
+                                           
+      winner(i)
 
 window = Tk()
 player = "X"
 list= []
+
+def winner(i):
+      win = ((0,1,2),
+             (3,4,5),
+             (6,7,8),
+             (0,3,6),
+             (1,4,7),
+             (2,5,8),
+             (0,4,8),
+             (2,4,6))
+      
+      for i in win:
+            if(list[i[0]]["text"] == list[i[1]]["text"] == list[i[2]]["text"] == "X"):
+                  tkinter.messagebox.showinfo("", "winner is X")
+                  quit()
+                  return
+            
+            elif(list[i[0]]["text"] == list[i[1]]["text"] == list[i[2]]["text"] == "O"):
+                  tkinter.messagebox.showinfo("", "winner is O")
+                  quit()
+                  return
 
 for i in range(9) :
       b = Button(window, text="     ", command=lambda k=i: checked(k))
